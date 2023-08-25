8aeb7cea4e7db7737ed41b899f1234c6cb5a4cd6
Asmit Kumar <69111936+Asmit108@users.noreply.github.com>
2023-08-25 13:30:44 +0530

    Update client.js

--- client_server/client.js
+++ client_server/client.js
@@ -3,8 +3,8 @@ const socket=io('http://localhost:8000');
 const form=document.querySelector('#frms');
 const messageInput=document.querySelector('.txtfrm')
 const messageContainer=document.querySelector('.container');
-var audio=new Audio('Message.mp3');
-const append=(message,position)=>{
+var audio=new Audio('Message.mp3');  
+const append=(message,position)=>{                             /*   for creating new message element at the time of receiving or sending messages or any user joined or left */    
     const messageElement=document.createElement('div');
     messageElement.innerText=message;
     messageElement.classList.add(position);
@@ -14,7 +14,7 @@ const append=(message,position)=>{
     }
 }
 
-form.addEventListener('submit',(e)=>{
+form.addEventListener('submit',(e)=>{                        /*  for taking the input message from the client when he wants to send */
     e.preventDefault();
     const message=messageInput.value;
     append(`You: ${message}`,'right')
@@ -25,12 +25,12 @@ form.addEventListener('submit',(e)=>{
 const name=prompt('Enter your name to join');
 socket.emit('new-user-joined',name);
 
-socket.on('user-joined',data=>{
+socket.on('user-joined',data=>{                             /* client is listening the event when someone joined the chat */
    append(`${data} joined the chat`,'left'); 
-});
-socket.on('receive',data=>{
+});    
+socket.on('receive',data=>{                                 /*  client is listening the event when someone is sending the message to client  */
    append(`${data.name}: ${data.message}`,'left');
 });
-socket.on('left',name=>{
+socket.on('left',name=>{                                   /*   client is listening the event when any user has left the chat  */
    append(`${name} left the chat`,'left');
 });
