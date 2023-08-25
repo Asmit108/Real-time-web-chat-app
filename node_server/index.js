eb9e97402e28200ec130b13c3da39e7edc05b0b1
Asmit Kumar <69111936+Asmit108@users.noreply.github.com>
2023-08-25 13:36:08 +0530

    Update index.js

--- node_server/index.js
+++ node_server/index.js
@@ -2,19 +2,19 @@ const cors=require('cors');
 const io =require('socket.io')(8000,{
   cors: {
     origin: '*',
-  }});
+  }});                                                               /*  creating socket.io server   */
 const users={};
-io.on('connection',socket=>{
-  socket.on('new-user-joined',name=>{
+io.on('connection',socket=>{                                         /*   making the server connection active  */
+  socket.on('new-user-joined',name=>{                                /*  server is listening the event when new user has joined the chat   */
     console.log(name);
     users[socket.id]=name;
-    socket.broadcast.emit('user-joined',name);
+    socket.broadcast.emit('user-joined',name);                       /* server is sending the message about new user joining chat to  every old clients  */
   });
-  socket.on('send',message=>{
-    socket.broadcast.emit('receive',{message:message,name:users[socket.id]});
+  socket.on('send',message=>{                                        /*  server is listening the event when new user has is sending the message   */  
+    socket.broadcast.emit('receive',{message:message,name:users[socket.id]});   /*  server is sending the message with user name(who has sent it) to every other client   */
   });
-  socket.on('disconnect',message=>{
-    socket.broadcast.emit('left',users[socket.id]);
+  socket.on('disconnect',message=>{                                  /*  server is listening the event when any user has left the chat   */
+    socket.broadcast.emit('left',users[socket.id]);                  /*  server is sending the message of user left the chat to every client  */
     delete users[socket.id];
   });
 });
